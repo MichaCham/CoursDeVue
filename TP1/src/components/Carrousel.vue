10cc59122c6352254094e034fc902745b8b08cfd
Michaël Champalet <cham7694-github@yahoo.com>
2022-02-08 16:22:59 +0100

    Maj Carrousel

--- TP1/src/components/Carrousel.vue
+++ TP1/src/components/Carrousel.vue
@@ -1,5 +1,8 @@
 <template>
+
+
   <img alt="Image" src={{listeImage[i]}}>
+  <button v-on:click="next"></button>
 </template>
 
 <script>
@@ -9,12 +12,31 @@
             return {
                 numerosImage: 0,
                 listeImage: ["../img/1.png","../img/2.png","../img/3.png","../img/4.png","../img/5.png","../img/6.png"],
+                monImage : listeImage[i]
             }
         },
         methods:{
+            next_right: function (){
+                this.numerosImage ++;
+                if(this.numerosImage >= this.listeImage.length()){
+                    this.numerosImage =0;
+                }
+            },
+            next_left: function (){
+                this.numerosImage --;
+                if(this.numerosImage < 0){
+                    this.numerosImage = this.listeImage.length();
+                }
+            }
         }
     }) 
 </script>
+
+<style>
+img {
+	width: 1000px;
+}
+</style>
 
 
 
